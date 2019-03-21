2c9f1d6868aae66256aad486033f6a4d7708f34d
ying.liu <yingliu@galaxys-MacBook-Pro.local>
2019-03-21 14:32:19 -0700

    revise goal robot

--- src/robots/goal.js
+++ src/robots/goal.js
@@ -1,5 +1,27 @@
 import { map } from '../map/map.js'
 
+let routes = []
+export function goGoal(state) {
+  let next
+  const { place, parcels } = state
+  if (routes.length === 0) {
+    findParcelRoute(place, parcels[0])
+  }
+  next = routes[0]
+  routes = routes.slice(1)
+  return next
+}
+
+function findParcelRoute(place, parcel) {
+  if (place === parcel.from) {
+    // find a route to deliver the parcel
+    routes = findRoute(place, parcel.to)
+  } else {
+    // find a route to get the parcel
+    routes = findRoute(place, parcel.from)
+  }
+}
+
 function findRoute(from, to) {
   let work = [{ at: from, routes: [] }]
   for (let i = 0; i < work.length; i++) {
@@ -14,28 +36,4 @@ function findRoute(from, to) {
       }
     }
   }
-}
-
-let routes = []
-export function goGoal(state) {
-  let next
-  const { place, parcels } = state
-  if (routes.length === 0) {
-    routes = findParcelRoute(place, parcels[0])
-  }
-  next = routes[0]
-  routes = routes.slice(1)
-  return next
-}
-
-function findParcelRoute(place, parcel) {
-  let routes
-  if (place === parcel.from) {
-    // find a route to deliver the parcel
-    routes = findRoute(place, parcel.to)
-  } else {
-    // find a route to get the parcel
-    routes = findRoute(place, parcel.from)
-  }
-  return routes
 }
